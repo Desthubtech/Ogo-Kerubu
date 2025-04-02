8e137812f271c1e1569f4c26bb67766ac9a25fb1
Desthubtech <dominiongraphics.publishing@gmail.com>
2025-04-02 13:30:05 -0700

    Ogo Kerubu-Serafu Project

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle the navigation menu on small screens
+document.getElementById('hamburger').addEventListener('click', function () {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+  });
+  
